c8b9f475d6dc0f88efc9d50495b2b85be4d6c459
LP2851 <lucas.phillips28@hotmail.com>
2022-04-30 20:34:48 +0100

    Merge Sort and  to track sorting

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import pygame
 import time
 import random
@@ -10,8 +11,6 @@ HEIGHT = 512
 
 SPACE_BETWEEN_BARS = 5
 
-FRAME_LENGTH = 0.1
-
 # COLORS
 BLACK   = (  0,   0,   0)
 WHITE   = (255, 255, 255)
@@ -20,13 +19,62 @@ GREEN   = (  0, 255,   0)
 BLUE    = (  0,   0, 255)
 
 
+class SortingElement:
+    def __init__(self, value: float) -> None:
+        self.value = value
+        self.focused = False
+
+    @staticmethod
+    def __other_is_sorting_element(other) -> bool:
+        return isinstance(other, SortingElement)
+
+    def __ge__(self, other):
+        if not self.__other_is_sorting_element(other):
+            raise TypeError
+        return self.value >= other.value
+
+    def __le__(self, other):
+        if not self.__other_is_sorting_element(other):
+            raise TypeError
+        return self.value <= other.value
+
+    def __lt__(self, other):
+        if not self.__other_is_sorting_element(other):
+            raise TypeError
+        return self.value < other.value
+
+    def __gt__(self, other):
+        if not self.__other_is_sorting_element(other):
+            raise TypeError
+        return self.value > other.value
+
+    def __eq__(self, other):
+        if not self.__other_is_sorting_element(other):
+            raise TypeError
+        return self.value == other.value
+
+    def __ne__(self, other):
+        if not self.__other_is_sorting_element(other):
+            raise TypeError
+        return self.value != other.value
+
+    def __int__(self):
+        return self.value
+
+    def __float__(self):
+        return self.value
+
+    def __repr__(self):
+        return str(self.value)
+
+
 class ShowSortingGUI(GUI):
-    def __init__(self, width: int, height: int, sort_name: str) -> None:
+    def __init__(self, width: int, height: int, sort_name: str, frame_length: float = 0.1) -> None:
         super().__init__(width, height)
         self._create_window(sort_name)
         self._to_sort = []
-        self.focused_indexes = []
         self.complete = False
+        self._frame_length = frame_length
 
     def start(self, wait_time: float) -> None:
         time.sleep(wait_time)
@@ -45,28 +93,28 @@ class ShowSortingGUI(GUI):
         for i, val in enumerate(self._to_sort):
             # LEFT, TOP, WIDTH, HEIGHT
             rect = pygame.Rect((i+1) * SPACE_BETWEEN_BARS + i * bar_width,
-                               self._height - val,
+                               self._height - val.value,
                                bar_width,
-                               val)
+                               val.value)
             if self.complete:
                 color = GREEN
             else:
-                color = GREEN if i in self.focused_indexes else WHITE
+                color = GREEN if val.focused else WHITE
+                val.focused = False
 
             pygame.draw.rect(self.window, color, rect)
 
-        self.focused_indexes.clear()
         pygame.display.update()
-        time.sleep(FRAME_LENGTH)
+        time.sleep(self._frame_length)
 
     def sort(self) -> None:
         pass
 
-    def generate_values_to_sort(self, amount: int) -> list[float]:
+    def generate_values_to_sort(self, amount: int) -> list[SortingElement]:
         max_height = self._height * 0.9
         out = []
         for _ in range(amount):
-            out.append(random.random() * max_height)
+            out.append(SortingElement(random.random() * max_height))
         return out
 
 
@@ -82,8 +130,8 @@ class BubbleSort(ShowSortingGUI):
         while changed:
             changed = False
             for i in range(len(self._to_sort)-1):
-                self.focused_indexes.append(i)
-                self.focused_indexes.append(i+1)
+                self._to_sort[i].focused = True
+                self._to_sort[i + 1].focused = True
                 if self._to_sort[i] > self._to_sort[i+1]:
                     self._to_sort[i], self._to_sort[i+1] = self._to_sort[i+1], self._to_sort[i]
                     changed = True
@@ -92,3 +140,63 @@ class BubbleSort(ShowSortingGUI):
         self.complete = True
         self.draw()
 
+
+class MergeSort(ShowSortingGUI):
+    def __init__(self, width: int, height: int, elements: int) -> None:
+        super().__init__(width, height, "Merge Sort", frame_length=0.02)
+        self._to_sort = self.generate_values_to_sort(elements)
+        self.draw()
+
+    def __merge(self, start: int, middle: int, end: int) -> None:
+        left = self._to_sort[start:middle+1]
+        right = self._to_sort[middle+1:end+1]
+
+        i = j = 0
+        k = start
+
+        while i < len(left) and j < len(right):
+            left[i].focused = True
+            right[j].focused = True
+            self.draw()
+            left[i].focused = True
+            right[j].focused = True
+            if left[i] < right[j]:
+                print(f"{left[i]}<{right[j]}")
+                self._to_sort[k] = left[i]
+                i += 1
+            else:
+                self._to_sort[k] = right[j]
+                j += 1
+            k += 1
+            self.draw()
+
+        while i < len(left):
+            left[i].focused = True
+            self.draw()
+            left[i].focused = True
+            self._to_sort[k] = left[i]
+            i += 1
+            k += 1
+            self.draw()
+
+        while j < len(right):
+            right[j].focused = True
+            self.draw()
+            right[j].focused = True
+            self._to_sort[k] = right[j]
+            j += 1
+            k += 1
+            self.draw()
+
+    def __merge_sort(self, start: int, end: int) -> None:
+        if start < end:
+            mid = int((start + end) / 2)
+            self.__merge_sort(start, mid)
+            self.__merge_sort(mid+1, end)
+
+            self.__merge(start, mid, end)
+
+    def sort(self) -> None:
+        self.__merge_sort(0, len(self._to_sort)-1)
+        self.complete = True
+        self.draw()
