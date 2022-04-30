39b8bcf0d01aab5d240ebd30114161911a0869aa
LP2851 <lucas.phillips28@hotmail.com>
2022-04-30 20:48:29 +0100

    Commented

--- gui.py
+++ gui.py
@@ -3,22 +3,36 @@ pygame.font.init()
 
 
 class GUI:
+    """
+    Class for creating a GUI object
+    """
     def __init__(self, width: int, height: int) -> None:
+        """
+        :param width: The width of the window
+        :param height: The height of the window
+        """
         self._width = width
         self._height = height
         self.window = None
 
     def _create_window(self, caption: str) -> None:
+        """
+        Creates the window
+        :param caption: The name of the window
+        """
         self.window = pygame.display.set_mode((self._width, self._height))
         pygame.display.set_caption(caption)
 
     def draw(self) -> None:
+        """
+        Redraws the window
+        """
         pass
 
     @staticmethod
     def check_events_exit() -> bool:
         """
-        Checks to see if a `pygame` `QUIT` event has triggered.
+        Checks to see if a `pygame` `QUIT` event has triggered
         :return: If there is a `pygame.QUIT` event
         :rtype: bool
         """
@@ -30,7 +44,7 @@ class GUI:
     @staticmethod
     def close() -> None:
         """
-        Closes the current Pygame window.
+        Closes the current Pygame window
         """
         pygame.quit()
         pygame.init()
