b51a4ad7b8e2b6706e1cca8178a65f7f96280503
LP2851 <lucas.phillips28@hotmail.com>
2022-04-30 20:45:51 +0100

    Commented

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ ELEMENTS_TO_SORT = 50
 
 
 def arg_help() -> None:
+    """
+    Runs the --help command
+    Prints all commands and their descriptions.
+    """
     print("Help:")
     print("Here is a list of commands you can use.")
     for arg, val in all_args.items():
@@ -18,6 +22,10 @@ def arg_help() -> None:
 
 
 def arg_bubble_sort() -> None:
+    """
+    Runs the --bubble-sort command
+    Runs and shows the user a bubble sort
+    """
     sorter = BubbleSort(768, 512, ELEMENTS_TO_SORT)
     sorter.start(0.1)
     while not sorter.check_events_exit():
@@ -26,6 +34,10 @@ def arg_bubble_sort() -> None:
 
 
 def arg_merge_sort() -> None:
+    """
+    Runs the --merge-sort command
+    Runs and shows the user a merge sort
+    """
     sorter = MergeSort(768, 512, ELEMENTS_TO_SORT)
     sorter.start(0.1)
     while not sorter.check_events_exit():
@@ -34,6 +46,10 @@ def arg_merge_sort() -> None:
 
 
 def arg_pathfinder() -> None:
+    """
+    Runs the --pathfinder command
+    Runs a pathfinding algorithm (WIP)
+    """
     pathfinder = ShowPathfindingGUI(768, 512, "test")
     pathfinder.draw()
     # while True:
@@ -44,15 +60,26 @@ def arg_pathfinder() -> None:
 
 
 def discard_arg(arg: str) -> None:
+    """
+    Tells the user that the arg found doesn't exist and has been discarded
+    :param arg: The command that doesn't exist
+    """
     # red, reset
     print(f"\u001b[31m[Command Error]: Discarded the command '{arg}'.\u001b[0m")
 
 
 def check_args(args: list[str]) -> list[str]:
+    """
+    Checks all the args passed, removes invalid ones and returns the remaining ones
+    :param args: All the arguments passed by the user
+    :return: The list of arguments that are valid
+    :rtype: list[str]
+    """
     possible_args = []
     arg_keys = all_args.keys()
 
     for arg in args:
+        # Making --help command run first
         if arg == "--help":
             possible_args = [arg] + possible_args
             continue
@@ -64,6 +91,10 @@ def check_args(args: list[str]) -> list[str]:
 
 
 def run(args: list[str]) -> None:
+    """
+    Runs all commands in the order they were entered
+    :param args: The valid arguments that will be run
+    """
     for arg in args:
         # bold, green, reset, green, reset
         print(f"\u001b[1m\u001b[32mRunning:\u001b[0m\u001b[32m {arg}\u001b[0m")
@@ -71,11 +102,16 @@ def run(args: list[str]) -> None:
 
 
 def main(args: list[str]) -> None:
+    """
+    Checks all the arguments and then runs the valid ones
+    :param args: All arguments from user
+    """
     args = check_args(args)
     print()
     run(args)
 
 
+# Dictionary containing for all commands: command -> [description, function]
 all_args = {
     "--help": ["Prints the help menu.",
                arg_help],
